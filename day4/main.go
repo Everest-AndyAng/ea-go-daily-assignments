0698429610f308e7eec07af0601e30ba0e5ee5c4
Andy Ang <aang@seekasia.com>
2022-12-24 12:26:04 +0800

    day4 part 2

--- day4/main.go
+++ day4/main.go
@@ -1,12 +1,63 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Book struct {
+	Id    int
+	Title string
+	Price float64
+}
 
 func main() {
+	r := setupRouter()
+
 	http.HandleFunc("/hello", helloHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	http.HandleFunc("/book", bookHandler)
+
+	r.Run(":8080")
+}
+
+func setupGin() *gin.Engine {
+	return gin.Default()
+}
+
+func setupRouter() *gin.Engine {
+	r := setupGin()
+
+	setupGetBooksRouter(r)
+	return r
+}
+
+func setupGetBooksRouter(r *gin.Engine) {
+	r.GET("/books", func(c *gin.Context) {
+		var books []Book
+		books = append(books, Book{1, "Learn Go", 19.90})
+		c.JSON(http.StatusOK, books)
+	})
+}
+
+func setupDeleteBooksRouter(r *gin.Engine) {
+	r.DELETE("/books/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(http.StatusOK, id)
+	})
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
+}
+
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	book := Book{1, "Learn Go", 19.90}
+
+	result, err := json.Marshal(book)
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	w.Write(result)
 }
--- day4/main_test.go
+++ day4/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -10,7 +12,7 @@ import (
 
 func TestHelloHandler(t *testing.T) {
 	rec := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/hello", nil)
+	r := httptest.NewRequest(http.MethodGet, "/hello", nil)
 
 	helloHandler(rec, r)
 
@@ -18,4 +20,49 @@ func TestHelloHandler(t *testing.T) {
 	data, _ := io.ReadAll(body)
 
 	assert.Equal(t, string(data), "hello")
+}
+
+func TestBookHandler(t *testing.T) {
+	rec := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/book", nil)
+
+	bookHandler(rec, r)
+
+	body := rec.Result().Body
+	data, _ := io.ReadAll(body)
+
+	var b Book
+	json.Unmarshal(data, &b)
+	expected := Book{1, "Learn Go", 19.90}
+	assert.Equal(t, b, expected)
+}
+
+func TestGetBooksRouterHandler(t *testing.T) {
+	router := setupGin()
+	setupGetBooksRouter(router)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/books", nil)
+	router.ServeHTTP(w, req)
+
+	body := w.Result().Body
+	data, _ := io.ReadAll(body)
+	var actual []Book
+	json.Unmarshal(data, &actual)
+
+	var expected []Book
+	expected = append(expected, Book{1, "Learn Go", 19.90})
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, actual, expected)
+}
+
+func TestDeleteBooksRouterHandler(t *testing.T) {
+	router := setupGin()
+	setupDeleteBooksRouter(router)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodDelete, "/books/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	// ToDo: delete books (better to separate as another test case but will need to use verify)
 }
