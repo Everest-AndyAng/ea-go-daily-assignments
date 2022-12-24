2e3412f99ed5a84f35d42b18cfe1fcc65e533ef2
Andy Ang <aang@seekasia.com>
2022-12-24 12:29:50 +0800

    Update Day3 comments

--- day3/helloWorld/main_test.go
+++ day3/helloWorld/main_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 )
 
+// Ideally, we wat to mock external dependencies and mock it's behavior
+// So that we don't depend on the behavior/implemntation, we just want to delegate
+// In order to do that, we would need to more complex implementation by introducing interface and mocking the depedency
 func TestGoQuote(t *testing.T) {
 	result := GoQuote()
 	assert.NotEmpty(t, result)
