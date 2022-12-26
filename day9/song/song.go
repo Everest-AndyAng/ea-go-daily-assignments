1603febb784268215a491a9d7f31f91c4f0d1335
Andy Ang <aang@seekasia.com>
2022-12-26 13:18:37 +0800

    Implement release date

--- day9/song/song.go
+++ day9/song/song.go
@@ -2,16 +2,17 @@ package song
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Song struct {
-	Id     int64  `json:"id"`
-	Title  string `json:"title"`
-	Singer string `json:"singer"`
-	Writer string `json:"writer"`
-	// Release_Date time.Time `json:"release_date"`
+	Id           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Singer       string    `json:"singer"`
+	Writer       string    `json:"writer"`
+	Release_Date time.Time `json:"release_date"`
 }
 
 type SongsController struct {
@@ -21,8 +22,8 @@ type SongsController struct {
 // Mocked DB
 func QuerySongs() []Song {
 	var songs []Song
-	songs = append(songs, Song{1, "In The End", "Linkin Park", "Linkin Park"})
-	songs = append(songs, Song{2, "From The Inside", "Linkin Park", "Linkin Park"})
+	songs = append(songs, Song{1, "In The End", "Linkin Park", "Linkin Park", time.Time{}})
+	songs = append(songs, Song{2, "From The Inside", "Linkin Park", "Linkin Park", time.Time{}})
 	return songs
 }
 
