63ae947d705dd398a840aa423c534f6e75ae7dfd
Andy Ang <aang@seekasia.com>
2022-12-26 15:53:20 +0800

    Update day2: PaytmWallet and refactor withdrawableAccount

--- day2/quickcash/quickcash.go
+++ day2/quickcash/quickcash.go
@@ -1,24 +1,30 @@
 package quickcash
 
+import "errors"
+
+// Assuming the order of accounts is always ordered
 type QuickCash struct {
 	Accounts []Withdrawable
 }
 
 func (qc *QuickCash) getCash(amount float64) (float64, string) {
-	withdrawableAccount := withdrawableAccount(qc, amount)
+	withdrawableAccount, _ := qc.withdrawableAccount(amount)
 
 	withdrawableAccount.WithDraw(amount)
 	return amount, withdrawableAccount.GetIdentifier()
 }
 
-func withdrawableAccount(qc *QuickCash, amount float64) Withdrawable {
+func (qc *QuickCash) withdrawableAccount(amount float64) (Withdrawable, error) {
 	for _, account := range qc.Accounts {
 		if account.GetIdentifier() == "SAVINGS_ACCOUNT" && account.CanWithDraw(amount) {
-			return account
+			return account, nil
 		}
 		if account.GetIdentifier() == "CREDITCARD_ACCOUNT" && account.CanWithDraw(amount) {
-			return account
+			return account, nil
+		}
+		if account.GetIdentifier() == "PAYTM_WALLET" && account.CanWithDraw(amount) {
+			return account, nil
 		}
 	}
-	return nil
+	return nil, errors.New("No funds avalable from all accounts")
 }
--- day2/quickcash/quickcash_test.go
+++ day2/quickcash/quickcash_test.go
@@ -20,7 +20,6 @@ func TestGetCashFromSavingsAccount(t *testing.T) {
 }
 
 func TestGetCashFromSecondaryAccount(t *testing.T) {
-
 	fpa := &SavingsAccount{300.0, 10.0}
 	fsa := &CreditCardAccount{0.0, 1000.0}
 
@@ -31,4 +30,47 @@ func TestGetCashFromSecondaryAccount(t *testing.T) {
 	amt, accType := fqc.getCash(500)
 	assert.Equal(t, float64(500), amt)
 	assert.Equal(t, fsa.GetIdentifier(), accType)
+}
+
+func TestGetCashFromPaytmWallet(t *testing.T) {
+	primaryAcc := &SavingsAccount{300.0, 10.0}
+	secondaryAcc := &PaytmWallet{500.0}
+
+	fqc := QuickCash{
+		[]Withdrawable{primaryAcc, secondaryAcc},
+	}
+
+	amt, accType := fqc.getCash(500)
+	assert.Equal(t, float64(500), amt)
+	assert.Equal(t, secondaryAcc.GetIdentifier(), accType)
+}
+
+func TestShouldReturnWithdrawableAccountWhenThereIsEnoughFunds(t *testing.T) {
+	primaryAcc := &SavingsAccount{300.0, 10.0}
+	secondaryAcc := &PaytmWallet{500.0}
+	tenaryAcc := &CreditCardAccount{-5000, 10000.0}
+
+	fqc := QuickCash{
+		[]Withdrawable{primaryAcc, secondaryAcc, tenaryAcc},
+	}
+
+	withdrawableAccount, _ := fqc.withdrawableAccount(2000)
+	assert.Equal(t, tenaryAcc, withdrawableAccount)
+}
+
+func TestShouldReturnErrorWhenThereIsNotEnoughFundsAvaialbleForWithdrawal(t *testing.T) {
+	primaryAcc := &SavingsAccount{300.0, 10.0}
+	secondaryAcc := &PaytmWallet{500.0}
+	tenaryAcc := &CreditCardAccount{-9000, 10000.0}
+
+	fqc := QuickCash{
+		[]Withdrawable{primaryAcc, secondaryAcc, tenaryAcc},
+	}
+
+	withdrawableAccount, err := fqc.withdrawableAccount(2000)
+
+	assert.Nil(t, withdrawableAccount)
+	if err == nil {
+		t.Errorf("Should return an error when there is not enough funds")
+	}
 }
